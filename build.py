b70bba602e07b4e197a04ca5803e550a3224675b
avi <db0zmail@gmail.com>
2021-11-30 11:20:52 +0300

    fixed build.py for macos

--- build.py
+++ build.py
@@ -20,50 +20,40 @@ os.rename("build/newlestehorn.zip", "build/newlestehorn.love")
 # copy files
 for platform in platforms:
     print(f"[{platform}]")
-    
+
     Path(f"build/{platform}").mkdir()
-    
+
     if platform == "windows":
         # copy love2d files
-        shutil.copytree("bin/windows/love", "build/windows", 
+        shutil.copytree("bin/windows/love", "build/windows",
             ignore=shutil.ignore_patterns("*.exe"),
             dirs_exist_ok=True)
-        
+
         # copy libraries
         for dll in ["nuklear.dll", "nfd.dll"]:
             shutil.copy(f"bin/windows/{dll}", "build/windows")
-        
+
         # concatenate love.exe and newlestehorn.love
         filenames = ["bin/windows/love/love.exe", "build/newlestehorn.love"]
         with open("build/windows/newlestehorn.exe", "wb") as of:
             for fn in filenames:
                 with open(fn, "rb") as inf:
                         of.write(inf.read())
-    elif platform == "linux":
+    else:
         # linux - no fancy packaging, just src and .so's
         # copy src
         shutil.copytree("build/src", f"build/{platform}", dirs_exist_ok=True)
-        
+
         # copy .so's
         for so in ["nuklear.so", "nfd.so"]:
             shutil.copy(f"bin/{platform}/{so}", f"build/{platform}")
-    elif platform == "macos":
-        # macos - no fancy packaging, just src and .so's
-        # copy src
-        shutil.copytree("build/src", f"build/{platform}", dirs_exist_ok=True)
-        
-        # copy .so and .py
-        # python file used as a hack for file dialog for macos
-        # since i couldn't compile nfd
-        for so in ["nuklear.so", "filedialog.py"]:
-            shutil.copy(f"bin/{platform}/{so}", f"build/{platform}")
 
-# create archives    
+# create archives
 version = input("version suffix: ")
 for platform in platforms:
     arcname = f"newlestehorn-{version}-{platform}"
     os.rename(f"build/{platform}", f"build/{arcname}")
-    
+
     fmt = "zip" if platform == "windows" else "gztar"
     shutil.make_archive(f"build/{arcname}", fmt, "build", arcname)
 
